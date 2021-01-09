a6b9d58248251b925455812b00024603c94706a2
liebealles <dywjdal1995@naver.com>
2021-01-09 19:45:00 +0900

    vue.js로 todo app 만들기

---
+++ vuestudy/src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mb-2 d-flex">
+    <div>
+      <label>
+        <input type="checkbox" :checked="todo.checked" @change="toggleCheckbox"/>
+      </label>
+    </div>
+    <span class="ml-3 flex-grow-1" :class="todo.checked ? 'text-muted' : ''"
+          :style="todo.checked ? 'text-decoration:line-through' : ''">{{ todo.text }}</span>
+    <button class="btn btn-danger btn-sm" @click="clickelete">delete</button>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    toggleCheckbox(e) {
+      this.$emit('toggle-checkbox', {
+        id: this.todo.id,
+        checked: e.target.checked
+      })
+    },
+    clickelete(){
+      this.$emit('click-delete', this.todo.id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuestudy/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    outputDir : "../test/src/main/resources/static",
+    indexPath: "../static/index.html"
+};
